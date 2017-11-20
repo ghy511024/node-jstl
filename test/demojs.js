1f9a7866bc9a58db0cc1968e3537351676ef723d
龚虹宇 <249398279@qq.com>
2017-11-21 02:08:12 +0800

    记录

---
+++ test/demojs.js
@@ -0,0 +1,45 @@
+class newjsp_js {
+    outjs() {
+        this.ret = "";
+        let _jspx_page_context = {};
+        try {
+            this.ret += "\r\n\n";
+            if (this._js_metch_c_forEach_0(_js_page_context)) {
+                this.ret;
+            }
+            this.ret += "\n"
+        }
+        catch (e) {
+
+        }
+    }
+
+    _js_metch_c_forEach_0(_js_page_context) {
+        let forEachIpml = new forEachIpml();
+
+        // 设置变量
+        forEachIpml.context = _js_page_context;
+        forEachIpml.parent = null;
+        forEachIpml.items = "list1";
+        forEachIpml.var = "item";
+        try {
+            let _js_eval_c_forEach_0 = forEachIpml.doStartTag();
+            if (_js_eval_c_forEach_0 != 0) {
+                do {
+                    //== head 部分输出完毕
+                    //== Body 部分开始输出
+                    this.ret += "nnn";
+                    //== body 输出完毕
+
+                    //=== end 部分开始输出
+                }
+                while (true)
+            }
+        }
+        catch (e) {
+
+        }
+        return false;
+        //=== end 部分开始结束
+    }
+}
