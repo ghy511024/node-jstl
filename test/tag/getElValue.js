480e1ebdb03b14fc990bb098c45ed1f2b65156dd
龚虹宇 <249398279@qq.com>
2017-11-22 23:55:16 +0800

    el 获取值，demo 完成

--- test/tag/getElValue.js
+++ test/tag/getElValue.js
@@ -9,15 +9,36 @@ class getElValue {
         exp.replace(reg, function (_, $1) {
             exp_str = $1;
         })
-        let tmpData = this.data;
+        let tmpData = this.data||{};
         let ret_value = null;
-        console.log("elvalue:", $1);
-        exp_str.split(".");
-        for (let i = 0; i < exp_str.length; i++) {
-            var c_key = exp_str[i];
-            ret_value = tmpData[c_key] || {};
+        console.log("elvalue:", exp_str);
+      var exp_array=  exp_str.split(".");
+        console.log(exp_str)
+        for (let i = 0; i < exp_array.length; i++) {
+            var c_key = exp_array[i];
+            ret_value = tmpData[c_key];
+
+            if (ret_value == null) {
+                break;
+            }
             tmpData = ret_value;
         }
+        console.log(ret_value);
         return ret_value;
     }
 }
+
+let d = {
+    str: "str11",
+    a1: [1, 2, "3"],
+    a2: [{a: "2"}],
+    o1: {ghy: "1"},
+    o2: {
+        o2_a1: ["ss", "s"],
+        o2_s1: "sdf",
+        o2_a3: [{bb: "sd"}],
+        o2_o1: {xixi: "xixistr"}
+    }
+}
+let a = new getElValue(d);
+a.getElValue("${o2.o2_o1.xixi}");
