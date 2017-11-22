24efd125d2326d42af89527604df241c49dc40ff
龚虹宇 <249398279@qq.com>
2017-11-22 23:45:39 +0800

    merge

--- test/tag/getElValue.js
+++ test/tag/getElValue.js
@@ -1,6 +1,6 @@
 class getElValue {
-    constructor() {
-
+    constructor(data) {
+        this.data = data || {};
     }
 
     getElValue(exp) {
@@ -9,11 +9,15 @@ class getElValue {
         exp.replace(reg, function (_, $1) {
             exp_str = $1;
         })
+        let tmpData = this.data;
+        let ret_value = null;
         console.log("elvalue:", $1);
         exp_str.split(".");
         for (let i = 0; i < exp_str.length; i++) {
             var c_key = exp_str[i];
-
+            ret_value = tmpData[c_key] || {};
+            tmpData = ret_value;
         }
+        return ret_value;
     }
 }
