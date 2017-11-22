438880beaa9c6ed38951f30787942e5b003915d2
龚虹宇 <249398279@qq.com>
2017-11-22 22:21:08 +0800

    servletWriter

---
+++ src/writer/ServletWriter.js
@@ -0,0 +1,17 @@
+const TAB_WIDTH = 2;
+const SPACES = "                          ";
+
+class ServletWriter {
+    constructor() {
+        this.indent = 0;
+        this.vertual_indent = 0;
+    }
+
+    printin() {
+
+    }
+
+    printlt() {
+
+    }
+}
